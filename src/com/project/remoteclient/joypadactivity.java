310a0649e9a3f8f3f29429d970321edcafc203f6
sabin262@gmail.com <sabin262@gmail.com>
2015-04-19 14:24:07 +0545

    Working joypad

--- src/com/project/remoteclient/joypadactivity.java
+++ src/com/project/remoteclient/joypadactivity.java
@@ -21,52 +21,49 @@ import android.view.View.OnTouchListener;
 
 
 public class joypadactivity extends Activity {
-	
+
 	int controlNumber;
-	 
+
 	ClientSocket client;
 	ToggleButton tb;
 	ImageView ibtnUP,ibtnCross,ibtnStart,ibtnSelect,ibtnSquare,ibtnTriangle, ibtnDown,ibtnLeft,ibtnRight,ibtnCircle;
-	
-			@Override
-			protected void onCreate(Bundle savedInstanceState) {
-				// TODO Auto-generated method stub
-				super.onCreate(savedInstanceState);
-				setContentView(R.layout.activity_joypad);
-				client= new ClientSocket ();
-				ibtnUP=(ImageView)findViewById(R.id.btnUp);
-				ibtnCross=(ImageView)findViewById(R.id.btnCross);
-				ibtnStart=(ImageView)findViewById(R.id.btnStart);
-				ibtnSelect=(ImageView)findViewById(R.id.btnSelect);
-				ibtnSquare=(ImageView)findViewById(R.id.btnSquare);
-				ibtnTriangle=(ImageView)findViewById(R.id.btnTriangle);
-				ibtnDown=(ImageView)findViewById(R.id.btnDown);
-				ibtnLeft=(ImageView)findViewById(R.id.btnLeft);
-				ibtnRight=(ImageView)findViewById(R.id.btnRight);
-				ibtnCircle=(ImageView)findViewById(R.id.btnCircle);
-				tb=(ToggleButton)findViewById(R.id.toggleButton1);
-				ibtnUP.setOnTouchListener(otl);
-				ibtnCross.setOnTouchListener(otl);
-				ibtnStart.setOnTouchListener(otl);
-				ibtnSelect.setOnTouchListener(otl);
-				ibtnSquare.setOnTouchListener(otl);
-				ibtnTriangle.setOnTouchListener(otl);
-				ibtnDown.setOnTouchListener(otl);
-				ibtnLeft.setOnTouchListener(otl);
-				ibtnRight.setOnTouchListener(otl);
-				ibtnCircle.setOnTouchListener(otl);
-			}
-			OnTouchListener otl=new OnTouchListener() {
-				@Override
-				public boolean onTouch(View v, MotionEvent event) {
-				int action = (event.getAction() & MotionEvent.ACTION_MASK);
-				int button=0;
-				try{controlNumber=Integer.parseInt( tb.getText().toString())-1;
-				}
-				catch(Exception e)
-				{
-					controlNumber=0;
-				}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_joypad);
+		client= new ClientSocket ();
+		ibtnUP=(ImageView)findViewById(R.id.btnUp);
+		ibtnCross=(ImageView)findViewById(R.id.btnCross);
+		ibtnStart=(ImageView)findViewById(R.id.btnStart);
+		ibtnSelect=(ImageView)findViewById(R.id.btnSelect);
+		ibtnSquare=(ImageView)findViewById(R.id.btnSquare);
+		ibtnTriangle=(ImageView)findViewById(R.id.btnTriangle);
+		ibtnDown=(ImageView)findViewById(R.id.btnDown);
+		ibtnLeft=(ImageView)findViewById(R.id.btnLeft);
+		ibtnRight=(ImageView)findViewById(R.id.btnRight);
+		ibtnCircle=(ImageView)findViewById(R.id.btnCircle);
+		tb=(ToggleButton)findViewById(R.id.toggleButton1);
+		ibtnUP.setOnTouchListener(otl);
+		ibtnCross.setOnTouchListener(otl);
+		ibtnStart.setOnTouchListener(otl);
+		ibtnSelect.setOnTouchListener(otl);
+		ibtnSquare.setOnTouchListener(otl);
+		ibtnTriangle.setOnTouchListener(otl);
+		ibtnDown.setOnTouchListener(otl);
+		ibtnLeft.setOnTouchListener(otl);
+		ibtnRight.setOnTouchListener(otl);
+		ibtnCircle.setOnTouchListener(otl);
+	}
+	OnTouchListener otl=new OnTouchListener() {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			int action = (event.getAction() & MotionEvent.ACTION_MASK);
+			int button=0;
+			controlNumber=Integer.parseInt( tb.getText().toString())-1;
+
+			if (controlNumber==0){
 				switch(v.getId())
 				{
 				case R.id.btnUp:
@@ -99,28 +96,67 @@ public class joypadactivity extends Activity {
 				case R.id.btnLeft:
 					button=Buttons.JOY_PAD_LEFT;
 					break;
-					
+
 				}
-				
-				
-				
-					switch(action)
-				
+			}
+			else
+			{
+				switch(v.getId())
 				{
-				case MotionEvent.ACTION_UP:
+				case R.id.btnUp:
+					button=Buttons.JOY_PAD_UP2;
+					break;
+				case R.id.btnDown:
+					button=Buttons.JOY_PAD_DOWN2;
+					break;
+				case R.id.btnCircle:
+					button=Buttons.JOY_PAD_CIRCLE2;
+					break;
+				case R.id.btnCross:
+					button=Buttons.JOY_PAD_CROSS2;
+					break;
+				case R.id.btnSquare:
+					button=Buttons.JOY_PAD_SQUARE2;
+					break;
+				case R.id.btnTriangle:
+					button=Buttons.JOY_PAD_TRIANGLE2;
+					break;
+				case R.id.btnStart:
+					button=Buttons.JOY_PAD_START2;
+					break;
+				case R.id.btnSelect:
+					button=Buttons.JOY_PAD_SELECT2;
+					break;
+				case R.id.btnRight:
+					button=Buttons.JOY_PAD_RIGHT2;
+					break;
+				case R.id.btnLeft:
+					button=Buttons.JOY_PAD_LEFT2;
+					break;
+
+				}
+			}
+
+
+
+
+			switch(action)
+
+			{
+			case MotionEvent.ACTION_UP:
 				client.send(Events.BUTTON_RELEASE+","+button);
 				break;
-				case MotionEvent.ACTION_DOWN:
+			case MotionEvent.ACTION_DOWN:
 				client.send(Events.BUTTON_PRESS+","+button);
 				break;
-				}
-				return false;
-				
-							}};
-							
-							
-							
-							
-			
+			}
+			return false;
+
+		}};
+
+
+
+
+
 }
-			
+
